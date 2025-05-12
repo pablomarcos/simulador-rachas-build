3729da7b24330ac0434fc5e7c8efb5b4e77e1286
P4blo <papagolfalfa@protonmail.com>
2025-05-12 10:59:22 +0200

    Arco en angulos

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Paragliverse-Simulador de rachas de viento-1.0.4";
+const cacheName = "Paragliverse-Simulador de rachas de viento-1.0.5";
 const contentToCache = [
-    "Build/5d729fc90f040545c5dd929119e87853.loader.js",
-    "Build/e028fde7c34e72ad52ea2a17cdfdca03.framework.js",
-    "Build/9be041982e74c58c3cf6a513a4cae50b.data",
-    "Build/b890c2918a46af5bda38309ae73e3f2c.wasm",
+    "Build/4ce3fb5b3c85b3aeaf9daaa799a43bdf.loader.js",
+    "Build/c2d0505f47c5a96e955a982896324419.framework.js",
+    "Build/9adaf80a1e95910985908b72142b607d.data",
+    "Build/653cb415ea9f55ac7fbde957a4c557cd.wasm",
     "TemplateData/style.css"
 
 ];
