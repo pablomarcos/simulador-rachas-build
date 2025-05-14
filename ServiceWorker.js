c8b0c554717c5257f4d88f87c163be4f93949dbf
P4blo <papagolfalfa@protonmail.com>
2025-05-15 00:35:03 +0200

    Localization made FR-ES

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Paragliverse-Simulador de rachas de viento-1.0.5";
+const cacheName = "Paragliverse-Simulador de rachas de viento-1.0.6";
 const contentToCache = [
-    "Build/4ce3fb5b3c85b3aeaf9daaa799a43bdf.loader.js",
-    "Build/c2d0505f47c5a96e955a982896324419.framework.js",
-    "Build/9adaf80a1e95910985908b72142b607d.data",
-    "Build/653cb415ea9f55ac7fbde957a4c557cd.wasm",
+    "Build/cc9e5e8bc8712b4210ed7717aca4d73c.loader.js",
+    "Build/235112e707645e9c8ad5540bba9ed611.framework.js",
+    "Build/3f0f376bb92adb7b55de214b1e978f7c.data",
+    "Build/59ca03dda184abdeffaa8ebf06539ea2.wasm",
     "TemplateData/style.css"
 
 ];
